2096db841d0f8649720709227c9d33fb4e687048
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-03-11 15:12:12 +0500

    init

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGODB_URI)
+      .then(() =>
+        console.log("Database is connected to ", process.env.MONGODB_URI)
+      );
+  } catch (error) {
+    console.log("Error connecting database : ", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
